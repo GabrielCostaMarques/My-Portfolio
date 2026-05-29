b0d1172bf5c27a4692913af80a0871aecd11e0ca
Gabriel Marques <gmarques@r11travel.com.br>
2026-05-29 15:52:57 -0300

    update yml delete another defineconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { createLogger, defineConfig } from 'vite';
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/My-Portfolio/'
